b34f3b79d29d33151b32de80ad8b6ea0cbdcd4db
Chang-CH <chang.ch4348@gmail.com>
2024-07-17 23:27:53 +0800

    fix turbocard, projects page

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -1,33 +1,40 @@
 import TurboCard from '@components/stdlib/card/TurboCard';
 import styles from './s.module.scss';
 import BgDots from '@components/backgrounds/BgDots';
+import { Link } from 'react-router-dom';
 
 function Projects() {
   return (
     <div className={styles.rootContainer}>
-      <TurboCard className={styles.cardProject}>
-        <h2>Home server</h2>
-        <p>
-          An old laptop serving as a home server, running a variety of docker
-          compose services.
-        </p>
-      </TurboCard>
-      <TurboCard className={styles.cardProject}>
-        <h2>Java-slang</h2>
-        <p>
-          An implementation of the Java Virtual Machine, written in typescript.
-        </p>
-      </TurboCard>
-      <a href="hireside.co">
-        <TurboCard className={styles.cardProject}>
-          <h2>Hireside</h2>
-          <p>
-            Submission for the Venture Initiation Program at NUS. A platform
-            that provides instant price estimates for contract services using
-            historical data.
-          </p>
-        </TurboCard>
-      </a>
+      <h1>Projects</h1>
+      <div className={styles.cardContainer}>
+        <Link to="/selfhosted" className={styles.cardLink}>
+          <TurboCard className={styles.cardProject}>
+            <h2>Home server</h2>
+            <p>An old laptop with port forwarded docker services.</p>
+          </TurboCard>
+        </Link>
+        <a
+          href="https://github.com/source-academy/java-slang"
+          className={styles.cardLink}
+        >
+          <TurboCard className={styles.cardProject}>
+            <h2>Java-slang</h2>
+            <p>
+              An implementation of the Java Virtual Machine, written in
+              typescript.
+            </p>
+          </TurboCard>
+        </a>
+        <a href="hireside.co" className={styles.cardLink}>
+          <TurboCard className={styles.cardProject}>
+            <h2>Hireside</h2>
+            <p>
+              A platform that provides price estimates for contract services.
+            </p>
+          </TurboCard>
+        </a>
+      </div>
       <BgDots />
     </div>
   );
