6e0f9f549b000e18f2e8ce52441db2210c3d7870
Chang-CH <chang.ch4348@gmail.com>
2024-12-10 19:12:52 +0800

    initialize testing

---
+++ src/components/stdlib/rating/StarRating/index.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+const StarRating = props => {
+  const _emptyIcon = '/icons/stars/empty.svg';
+  const _filledIcon = '/icons/stars/filled.svg';
+  const _halfFilledIcon = '/icons/stars/half.svg';
+
+  // Utility function to calculate if the mouse event happened on the left side of the target or the right side.
+  const isLessThanHalf = event => {
+    const { target } = event;
+    const boundingClientRect = target.getBoundingClientRect();
+    let mouseAt = event.clientX - boundingClientRect.left;
+    mouseAt = Math.round(Math.abs(mouseAt));
+    return mouseAt <= boundingClientRect.width / 2;
+  };
+
+  const renderSymbol = () => {
+    return (
+      <img
+        src=""
+        className="rating-image"
+        data-testid="rating-icon"
+        alt="Rate"
+      />
+    );
+  };
+
+  return (
+    <div
+      tabIndex={0}
+      className="star-rating"
+      data-testid="star-rating-container"
+    >
+      {renderSymbol()}
+    </div>
+  );
+};
+
+export default StarRating;
